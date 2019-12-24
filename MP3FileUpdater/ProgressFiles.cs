2f581072d98a88d882e5e02f0cffe19c767964ad
Alexej <alexejdrosdov@gmail.com>
2019-12-24 09:45:15 +0300

    New commit

---
+++ MP3FileUpdater/ProgressFiles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MP3FileUpdater.Core
+{
+    public class ProgressFiles
+    {
+        public int ReadedFiles { get; set; }
+
+        public int ReamainingFiles { get; set; }
+
+        public Mp3File mp3File { get; set; }
+    }
+}
